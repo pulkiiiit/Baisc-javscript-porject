cd208589c4163932bbe8a08d97bf47b847c7a7b2
Pulkit Shukla <pulkitshukla001@gmail.com>
2025-03-26 23:55:28 +0530

    initial commit

---
+++ randum number generator/script.js
@@ -0,0 +1,14 @@
+// Random number generator 
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel")
+
+let min = 1 ;
+let max = 6;
+let randomNum ;
+ 
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min ;
+    myLabel.textContent = randomNum; 
+}
+ 
